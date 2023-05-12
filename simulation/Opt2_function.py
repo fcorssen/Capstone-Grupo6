9d21e2e71e65a239f0933acefcbb1927d4174912
ArielGoldzweig <agoldzweig@uc.cl>
2023-05-12 11:41:34 -0400

    comment 2opt

--- simulation/Opt2_function.py
+++ simulation/Opt2_function.py
@@ -26,8 +26,10 @@ def opt2(tour):
                 new2 = (tour[i+1], tour[(j+1)%n])
                 new_length = distance(tour[i], tour[j]) + distance(tour[i+1], tour[(j+1)%n])
 
+                # Reviso si al cambiar los nodos la distancia es menor
                 if new_length < cur_length:
                     # print(f'sawp {cur1} {cur2} with {new1} {new2}')
+                    # Los nodos que estan entre los nodos escogicos se invierten
                     tour[i+1:j+1] = tour[i+1:j+1][::-1]
                     tour_edges = [(tour[i], tour[i + 1]) for i in range(n)]
                     improved = True
